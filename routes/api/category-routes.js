28ed9eb1e1dcd0aafbee1bfcc3824973bc3048b9
avilwock <151412286+avilwock@users.noreply.github.com>
2024-03-23 15:13:59 -0700

    updated post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,15 +29,15 @@ try {
 }
 });
 
-// router.post('/', async (req, res) => {
-//   // create a new category
-//   try {
-//     const categoryData = await Category.create(req.body);
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.put('/:id', async (req, res) => {
 //   // update a category by its `id` value
